ade7fdf6e34b20b041db94808f6312f467995710
AnnaBakanina <bakaninaanya@gmail.com>
2025-04-13 11:49:16 +0200

    updating user profile

--- src/app/components/loggedInUser/user-profile/user-profile.component.ts
+++ src/app/components/loggedInUser/user-profile/user-profile.component.ts
@@ -3,20 +3,27 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { UserListingComponent } from '../user-listing/user-listing.component';
+import { UserInfoComponent } from '../user-info/user-info.component';
+import { AdminFormComponent } from '../../admin/admin-form/admin-form.component';
 // import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [CommonModule, UserListingComponent, RouterModule, VehicleTableListingComponent],
+  imports: [CommonModule, UserListingComponent, RouterModule, VehicleTableListingComponent, UserInfoComponent, AdminFormComponent],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
   // user: any = {};
   userName = 'Anna';
-  userEmail = 'annatest@test.com'
+  userEmail = 'annatest@test.com';
+  activeTab: string = 'info';
   
   // constructor(private userService: UserService) {}
+
+  setTab(tab:string) {
+    this.activeTab = tab;
+  }
 
   // ngOnInit(): void {
   //   this.userService.getUserProfile().subscribe(data => {
