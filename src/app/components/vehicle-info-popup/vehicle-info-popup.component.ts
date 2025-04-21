b54dfa2137daa7ebe7a3f71428687e80ccf7c0ac
AnnaBakanina <bakaninaanya@gmail.com>
2025-04-21 13:18:16 +0200

    updatin popup and adding logic to close it

--- src/app/components/vehicle-info-popup/vehicle-info-popup.component.ts
+++ src/app/components/vehicle-info-popup/vehicle-info-popup.component.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vehicle-info-popup',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './vehicle-info-popup.component.html',
   styleUrl: './vehicle-info-popup.component.css'
 })
 export class VehicleInfoPopupComponent {
   @Input() vehicle: any;
-  @Input() onClose!: () => void;
+  @Output() onClose = new EventEmitter<void>();
+
+  closePopup(): void {
+    this.onClose.emit();
+  }
 }
