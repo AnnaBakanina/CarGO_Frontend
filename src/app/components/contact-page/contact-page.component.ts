335251a317c39551cc0172619fb909521f908db2
AnnaBakanina <bakaninaanya@gmail.com>
2025-05-17 22:46:51 +0200

    wrapping up contact form

--- src/app/components/contact-page/contact-page.component.ts
+++ src/app/components/contact-page/contact-page.component.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
+import emailjs from '@emailjs/browser';
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.css'
 })
 export class ContactPageComponent {
+contactEmail = 'contact.us@cargo.com';
 
+  constructor(private toastr: ToastrService) {
+    emailjs.init('Mw4TGFbfUw0yqGkqk');
+  }
+
+  sendEmail(event: Event) {
+    event.preventDefault();
+
+    emailjs.sendForm(
+      'service_8daunzg',
+      'template_04iouuo',
+      event.target as HTMLFormElement,
+      'Mw4TGFbfUw0yqGkqk'
+    ).then(() => {
+      this.toastr.success('Лист надіслано', 'Готово!');
+    }).catch((error) => {
+      console.error('Помилка при надсиланні листа:', error);
+      this.toastr.error('Щось пішло не так...', 'Упс!');
+    });
+  }
 }
