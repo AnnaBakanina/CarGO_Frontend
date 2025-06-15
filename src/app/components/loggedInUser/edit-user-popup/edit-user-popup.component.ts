d0bce854b84de5f11ec363a42280636a56f70a25
AnnaBakanina <bakaninaanya@gmail.com>
2025-06-15 09:49:18 +0200

    update user from pop-up

--- src/app/components/loggedInUser/edit-user-popup/edit-user-popup.component.ts
+++ src/app/components/loggedInUser/edit-user-popup/edit-user-popup.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../../../services/user.service';
+import { User } from '../../../models/user';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-edit-user-popup',
@@ -10,15 +12,53 @@ import { UserService } from '../../../services/user.service';
   styleUrl: './edit-user-popup.component.css'
 })
 
-export class EditUserPopupComponent {
-  @Input() user: any;
+export class EditUserPopupComponent implements OnChanges {
+  @Input() user: User | null = null;
   @Output() onClose = new EventEmitter<void>();
 
-  constructor(private userService: UserService) {}
+  editableUser: User | null = null;
+  originalUser: User | null = null;
+
+  constructor(
+    private userService: UserService,
+    private toastr: ToastrService
+    ) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['user'] && this.user) {
+      this.originalUser = { ...this.user };
+      this.editableUser = { ...this.user };
+    }
+  }
 
   closePopup(): void {
     this.onClose.emit();
   }
 
-  saveChanges(){}
+  saveChanges(){
+    if (!this.editableUser) return;
+
+    this.userService.userDetails = {
+      ...this.userService.userDetails,
+      firstName: this.editableUser.firstName,
+      lastName: this.editableUser.lastName,
+      email: this.editableUser.email,
+      phoneNumber: this.editableUser.phoneNumber
+    };
+  
+    this.userService.updateProfile().subscribe({
+      next: () => {
+        this.toastr.success('Дані користувача оновлено', 'Готово!');
+        this.onClose.emit();
+      },
+      error: (err: any) => {
+        console.error(err);
+        this.toastr.error('Щось пішло не так...', 'Упс!');
+      }
+    });
+  }
+
+  isChanged(): boolean {
+    return JSON.stringify(this.editableUser) !== JSON.stringify(this.originalUser);
+  }
 }
